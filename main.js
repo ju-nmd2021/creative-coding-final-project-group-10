a8d371f3f34bbb3fe5087508fd7f63d6bfd7134a
lailata <93602827+lailata@users.noreply.github.com>
2023-09-28 10:46:26 +0200

    Styling video

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-let video;
 const synth = new Tone.MonoSynth().toDestination();
 const pingPongDelay = new Tone.PingPongDelay("4n", 0.5).toDestination();
-const seq = new Tone.Sequence((time, tone) => {
-  synth.triggerAttackRelease(tone, 0.8, time);
-}, ["C2", ["E2", "D2", "E2"], "D1", ["G1", "C1"]]);
+const seq = new Tone.Sequence(
+  (time, tone) => {
+    synth.triggerAttackRelease(tone, 0.8, time);
+  },
+  ["C2", ["E2", "D2", "E2"], "D1", ["G1", "C1"]]
+);
 Tone.Transport.start();
-
 
 synth.connect(pingPongDelay);
 pingPongDelay.toDestination();
@@ -24,16 +25,16 @@ const continueButton = document.getElementById("continueButton");
 startBeatButton.addEventListener("click", generateBeat);
 
 function generateBeat() {
-  document.getElementById("demo").innerHTML = "Hello World";
-    
-    Tone.start();
+  // document.getElementById("demo").innerHTML = "Hello World";
+
+  Tone.start();
 
   seq.start(0);
-}  
+}
 
 continueButton.addEventListener("click", startCamera);
 
 function startCamera() {
-//   document.getElementById("test").innerHTML = "Camera";
-  image(video, 0, 0, 640, 480);
+  //   document.getElementById("test").innerHTML = "Camera";
+  image(video, 350, 70, 640, 480);
 }
