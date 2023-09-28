94cac66f929553563b380392f9479984f91e2387
lailata <93602827+lailata@users.noreply.github.com>
2023-09-28 10:12:58 +0200

    Update Home Button and added video on click
    Buttons on beat.html now working on click

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+let video;
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+}
+
 const startBeatButton = document.getElementById("startBeatButton");
 
-startBeatButton.addEventListener("click", myFunction)
+const continueButton = document.getElementById("continueButton");
 
-function myFunction() {
+startBeatButton.addEventListener("click", generateBeat);
+
+function generateBeat() {
   document.getElementById("demo").innerHTML = "Hello World";
+}
+
+continueButton.addEventListener("click", startCamera);
+
+function startCamera() {
+//   document.getElementById("test").innerHTML = "Camera";
+  image(video, 0, 0, 640, 480);
 }
