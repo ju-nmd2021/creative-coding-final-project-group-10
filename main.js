7320d620ec600d32e0f70130a3e0ea5d0699ef63
LukesHoodie <klla20zl@student.ju.se>
2023-09-28 10:27:30 +0200

    attempt at tone

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 let video;
+const synth = new Tone.MonoSynth().toDestination();
+const pingPongDelay = new Tone.PingPongDelay("4n", 0.5).toDestination();
+const seq = new Tone.Sequence((time, tone) => {
+  synth.triggerAttackRelease(tone, 0.8, time);
+}, ["C2", ["E2", "D2", "H2"], "D1", ["G1", "C1"]]);
+Tone.Transport.start();
+
+synth.connect(pingPongDelay);
+pingPongDelay.toDestination();
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
@@ -15,7 +24,11 @@ startBeatButton.addEventListener("click", generateBeat);
 
 function generateBeat() {
   document.getElementById("demo").innerHTML = "Hello World";
-}
+  
+    Tone.start();
+
+  seq.start(0);
+}  
 
 continueButton.addEventListener("click", startCamera);
 
