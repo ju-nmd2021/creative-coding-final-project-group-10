739323dbb5b1f3fb9eb81adec189beeb75b6409c
LukesHoodie <klla20zl@student.ju.se>
2023-10-12 10:53:42 +0200

    tone works!

--- main.js
+++ main.js
@@ -100,14 +100,12 @@ function draw() {
 
     //Map distance to play in tones
     mappedDistance = map(distance / w, 0, 1, 0.2, 2.0);
-    if (mappedDistance < 0.8) {
+    if (mappedDistance > 0.8) {
       const synth = new Tone.Synth().toMaster();
       if (Tone.context.state !== "running") {
         Tone.start();
       }
       synth.triggerAttackRelease("G2", "8n");
-    } else {
-      Tone.stop();
     }
     }
   }
